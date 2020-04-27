2ba2cbe612e2c2929f917f0fe58e0003919e4679
gSchroeder <guto_schroeder@hotmail.com>
2020-04-26 22:57:26 -0300

    Chance message forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,22 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/998134c2c496298e1205ac043016acce/' + latitude + ',' + longitude +'?units=si'
+   const url = 'https://api.darksky.net/forecast/998134c2c496298e1205ac043016acce/' + latitude + ',' + longitude + '?units=si'
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-           callback('Sorry... Unable to connect to the DarkSky Weather API', undefined)
-        } else if(body.error){
-           callback('Sorry... Unable to find location', undefined)
-        } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' out. There is ' + body.currently.precipProbability + '% chance of rain.')
-        }
-     })
+   request({ url, json: true }, (error, { body }) => {
+      if (error) {
+         callback('Sorry... Unable to connect to the DarkSky Weather API', undefined)
+      } else if (body.error) {
+         callback('Sorry... Unable to find location', undefined)
+      } else {
+         callback(undefined, body.daily.data[0].summary +
+            ' It is currently ' + body.currently.temperature +
+            '. The high today is ' + body.daily.data[0].temperatureHigh +
+            ' with a low of ' + body.daily.data[0].temperatureLow +
+            '. There is ' + body.currently.precipProbability +
+            '% chance of rain.')
+      }
+   })
 }
 
 module.exports = forecast
